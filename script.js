5ad24d6bed8fa3d9c3be784ce6963ebf3acbe0ae
ColinLGR <161800308+ColinLGR@users.noreply.github.com>
2025-11-17 15:21:10 -0600

    Udated the database to contain real books

--- script.js
+++ script.js
@@ -5,26 +5,38 @@ const search = document.getElementById("searchBox");
 const filter = document.getElementById("Filters");
 const searchList = document.getElementById("searchedItem");
 
-const database = [[null,"name","$10.00","seller","description"],
-                    [null,"name1","$20.00","seller","description"],
-                    [null,"name2","$30.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name3","$40.00","seller","description"],
-                    [null,"name4","$50.00","seller","description"]];
+const database = [[null,"Grant's Tutoring: Calculus Two","$10.00","Marcus G","Grants Tutoring calculus 2 book, great condition, minimal wear."],//Calc 2 book
+		//calc 1 book
+                    [null,"James Stewart: Calculus, eighth edition","$20.00","Mary B","Intro to calculus book for MATH1500, like-new condition."],
+		//pysch books
+                    [null,"Garry L. Martin: Applied Sport Psychology","$30.00","Patrick S","Sport Psychology book for PSYCH1200, fair condition, worn spine."],
+		    [null,"Beth Morling: Research Methods in Psychology","$40.00","Garry V","Texbook for PSYC2250, used condition."],
+		//ECON books
+                    [null,"Paul Krugman: Microeconomics: Canadian Edition","$40.00","James P","Texbook for ECON1010, great condition, no damage."],
+                    [null,"N. Gregory Mankiw: Macroeconomics: Twelfth Edition","$40.00","Natalia P","Textbook for ECON2020, like-new condition"],
+		//Math book
+                    [null,"John A. Beachy: Abstract Algebra","$40.00","Minnette F","Texbook for MATH3320, mint condition."],
+                    [null,"Howard Anton: Elementary Linear Algebra","$40.00","Jeff N","Texbook for MATH1300, used condition."],
+		//Biology book
+                    [null,"Marieb Human Anatomy & Physiology","$40.00","Franklin B","Textbook for BIOL1410, great condition, no rips."],
+                    [null,"Ray Evert: Raven Biology of Plants","$100.00","Chloe B","Hardcover texbook for BIOL2240, perfect condition."],
+		//Russian book
+                    [null,"Robin: Golosa 1","$60.00","Colin L","Texbook for RUSN1302, used condition, writing in book."],
+		//physics book
+                    [null,"David Young: Physics","$40.00","Edward V","Textbook for PHYS1020, like-new condition"],
+		//Geology
+                    [null,"Reed Wincaster: Historical Geology","$35.00","Jordan G","Textbook for GEOL1400, fair condition."],
+		//Marketing
+                    [null,"Principles of Marketing","$20.00","Edward V","Textbook for MKT2210, like-new condition."],
+		//Astronomy
+                    [null,"Chaisson McMillan: Astronomy Today","$40.00","Colin L","Textbook for ASTR1810, used condition."],
+		//Chem
+                    [null,"Chemistry in Context: Applying Chemistry to Society","$38.00","Suki W","Textbook for CHEM1018, poor condition"],
+		//English
+                    [null,"Practical Grammar: A Canadian Writer's Resource","$15.00","Mona G","Textbook for ENGL0930, great condition."],
+                    [null,"William Shakespeare: Romeo and Juliet","$2.00","John B","Required reading for ENGL1200, poor condition."],
+		//Engineering
+                    [null,"Engineering Mechanics: Statics, 9th Edition","$50.00","Suki W","Texbook for ENG1440, like-new condition"]];
 
 filterList.forEach((b) => {
     const choosenFilters = document.getElementById("choosenFilters");
