7a91d8eeb07195d7bffae09efd006e21a08d9be7
ShadowChameleonLizard <Makis1@myumanitoba.ca>
2025-11-20 00:58:50 -0500

    Updated for study buddy contact information
    Allows for contact information to be shown and hidden

--- listings.js
+++ listings.js
@@ -12,3 +12,29 @@ for (let i = 0; i< imagePreviews.length; i++) {
         document.getElementById("main-photo").setAttribute("src", imageURL);
     })
 }
+
+let contactButtons = document.getElementsByClassName("contact-button");
+let contactPopups = document.getElementsByClassName("contact-popup");
+
+//Show the contact info of the profile that was clicked and hide all the other ones
+for (let i = 0; i< contactButtons.length; i++) {
+    contactButtons[i].addEventListener("click", () => {
+        contactPopups[i].setAttribute("style", "display: block");
+        for(let j = 0; j< contactButtons.length; j++)
+        {
+            if(j != i)
+            {
+                contactPopups[j].setAttribute("style", "display: none");
+            }
+        }
+    })
+}
+
+let closeBuddyPopups = document.getElementsByClassName("close-buddy-popup");
+
+//Close the contact info popup
+for (let i = 0; i< closeBuddyPopups.length; i++) {
+    closeBuddyPopups[i].addEventListener("click", () => {
+        contactPopups[i].setAttribute("style", "display: none");
+    })
+}
