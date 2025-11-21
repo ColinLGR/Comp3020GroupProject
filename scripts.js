74511adc326d0df7579ad86ca9b0cbc8779c48bb
finona4 <123525139+finona4@users.noreply.github.com>
2025-11-21 01:14:36 -0600

    aaaa

--- scripts.js
+++ scripts.js
@@ -1,45 +1,108 @@
 const defaultImg="/res/default_picture.png";
-const previewImage = (event) => {
-const theimage = document.getElementById("imgPreview1");
-theimage.style.display = "";
-const files = event.target.files;
-if(files.length > 0) {
-    // for (file in files)
-    // {
-        var imageUrl = URL.createObjectURL(files[0])
-        var imageEl = document.getElementById("imgPreview1")
-        imageEl.src = imageUrl;
-    // }
+var imageUrl0 = "";
+var imageEl0;
+const maxImgs = 3;
+let imgs = [maxImgs];
+let imgCounter = 0;
+let currImage = -1;
 
+const previewImage = (event) => {
+    const theimage = document.getElementById("imgPreview0");
+    theimage.style.display = "";
+    const files = event.target.files;
+    if(files.length > 0) {
+        imageUrl0 = URL.createObjectURL(files[0])
+        imageEl0 = document.getElementById("imgPreview0")
+        imageEl0.src = imageUrl0;
+        //currImage=-1;
+    }
 }
-let maxImgs = 10;
-let imgs = [10];
-if(imgCounter<maxImgs-1) {
-    imgs[imgCounter] = imageUrl;
-}
-console.log(imgs[imgCounter]);
-if (imgCounter < maxImgs)
-{
-    imgCounter++;
-    console.log(imgCounter);
-    
-    const fullId = "imageUpload"+String(imgCounter)
-    const fullName = "image"+String(imgCounter)
-    const fullPrevId = "imgPreview"+String(imgCounter)
-}
+
+const addImage = (event) => {
+    if (imageUrl0 != "")
+    {
+        const imgFile = document.getElementById('imageUpload1');
+        const imgPreview = document.getElementById("imgPreview0");
+        if(imgCounter<maxImgs-1) {
+            imgs[imgCounter] = imageUrl0;
+        }
+        console.log(imgs[imgCounter]);
+        if (currImage != -1)
+        {
+            const fullPrevId = "imgPreview"+String(currImage);
+
+            var imageEl = document.getElementById(fullPrevId);
+            imageEl.src = imageUrl0;
+        }
+        else if (imgCounter < maxImgs)
+        {
+            imgCounter++;
+            console.log(imgCounter);
+            
+            const fullPrevId = "imgPreview"+String(imgCounter);
+
+            var imageEl = document.getElementById(fullPrevId);
+            imageEl.src = imageUrl0;
+        }
+        imageUrl0 = "";
+        imgFile.value = '';
+        imgPreview.src=defaultImg;
+    }
 }
 
 const removeImage = (event) => {
+    const imgPreview = document.getElementById("imgPreview0")
+    if (imgPreview.src != defaultImg)
+    {
+        const imgFile = document.getElementById('imageUpload1')
+        imageUrl0 = "";
+        imgFile.value = '';
+        imgPreview.src=defaultImg;
 
-const remove1 = document.getElementById('imageUpload1')
-const remove2 = document.getElementById("imgPreview1")
-remove1.value = '';
-//remove2.style.display = 'none';
-remove2.src=defaultImg;
+        if (currImage != -1)
+        {
+            const fullPrevId = "imgPreview"+String(currImage);
+            const removeImg = document.getElementById(fullPrevId);
+            removeImg.src=defaultImg;
+            currImage=-1;
+            imgCounter--;
+        }
+        console.log(imgCounter);
+    }
+
+        
 }
 
-const slideshow = (event) => {
+function viewBtnImage() {
+    const fullPrevId = "imgPreview"+String(currImage)
+    const theimage = document.getElementById(fullPrevId);
 
+    var imageUrl = theimage.src;
+    imageEl0 = document.getElementById("imgPreview0")
+    imageEl0.src = imageUrl;
+
+}
+
+const setButton1 = (event) => {
+    // if (document.getElementById("imgPreview1").src != defaultImg)
+    // {
+        currImage = 1;
+        viewBtnImage();
+    // }
+}
+const setButton2 = (event) => {
+    // if (document.getElementById("imgPreview2").src != defaultImg)
+    // {
+        currImage = 2;
+        viewBtnImage();
+    // }
+}
+const setButton3 = (event) => {
+    // if (document.getElementById("imgPreview3").src != defaultImg)
+    // {
+        currImage = 3;
+        viewBtnImage();
+    // }
 }
 /*
 let imgCounter = 0;
@@ -73,10 +136,10 @@ input1.onchange = previewImage(event);
 let img =document.createElement('img');
 img.id=fullPrevId;*/
 
-div.appendChild(label1);
-div.appendChild(input1);
-div.appendChild(img);
-document.body.appendChild(div);
+// div.appendChild(label1);
+// div.appendChild(input1);
+// div.appendChild(img);
+// document.body.appendChild(div);
 
 /*
 return(
